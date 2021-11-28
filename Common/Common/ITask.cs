d071c1b4bf19ed73c77385af25c9c45d4f0898d7
cloverfeng <clover.feng@tpisoftware.com>
2021-11-28 11:08:32 +0800

    新增共用類別庫、共用介面

---
+++ Common/Common/ITask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// 課程專案介面
+    /// </summary>
+    public interface ITask
+    {
+        /// <summary>
+        /// 執行專案方法
+        /// </summary>
+        void RunTask();
+    }
+}
